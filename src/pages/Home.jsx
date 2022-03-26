1773850551114095d7f825195321853a8b347964
nub69 <shabik.ashab@gmail.com>
2022-03-26 12:00:20 +0600

    initial

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../componets/Navbar'
+
+const container = 
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Home

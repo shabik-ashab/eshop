f79ab3d1d63fa0f420d97bb06ef083acac088bad
nub69 <shabik.ashab@gmail.com>
2022-05-25 19:34:31 +0600

    cart added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return <div>
-    <Register />
+    <Home />
   </div>;
 };
 

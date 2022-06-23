b3983b9e8c457205c48237ff38087cd98075ff22
nub69 <shabik.ashab@gmail.com>
2022-06-23 17:17:32 +0600

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,30 @@ import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+// import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+// import Success from "./pages/Success";
 
-const App = () => {
-  return <div>
-    <Home />
-  </div>;
-};
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category"  element={<ProductList />} />
+          <Route path="/product/:id" element={<Product />}></Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
 
 export default App;
